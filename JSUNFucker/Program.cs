05ca9e3ace633eaa963006cfdbfc72e73e514585
Farzad E. <farzade@gmail.com>
2015-01-14 23:40:01 -0800

    Added output sanitation, minimally tested for now

---
+++ JSUNFucker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JSUNFucker
+{
+    internal class Program
+    {
+        readonly static Regex r = new Regex("([^\\+])\\+(?!\\+)", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2) Exit("Usage: JSUNFucker.exe <JSFuck Encrypted File> <Output Filename>");
+            else if (!File.Exists(args[0])) Exit("Cannot locate the specified source file ! :(");
+            try
+            {
+                var srcFile = File.ReadAllText(args[0]);
+                srcFile = Dictionary.JSFuckDict.Aggregate(srcFile,
+                    (current, pair) => current.Replace(pair.Key, pair.Value));
+                string res = r.Replace(srcFile, "$1").Replace("[][filter][constructor](", "");
+                File.WriteAllText(args[1], res.Substring(res.Length - 3) == ")()" ? res.Substring(0, res.Length - 3) : res);
+            }
+            catch (Exception ex)
+            {
+                Exit("An error occurred: " + ex.Message);
+            }
+        }
+
+        private static void Exit(string msg)
+        {
+            Console.WriteLine(msg);
+            Environment.Exit(0);
+        }
+    }
+}
