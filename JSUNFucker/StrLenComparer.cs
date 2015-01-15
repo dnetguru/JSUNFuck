f4e868039337e9fe223c4ee0239c404ce09c4029
Farzad E. <farzade@gmail.com>
2015-01-14 23:30:07 -0800

    Initial commit !

---
+++ JSUNFucker/StrLenComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace JSUNFucker
+{
+    public class StrLenComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Length == y.Length)
+                return 1;
+            return y.Length - x.Length;
+        }
+    }
+}
